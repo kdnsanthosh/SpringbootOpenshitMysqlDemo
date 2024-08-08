f2b211b39e10d8c7a09a4853979848ddc770f024
Jilla Santhosh <kdnsa@SANTHOSH.YC.WORKSPACE>
2024-08-08 12:02:03 +0300

    modified

--- src/main/java/com/codecraftshop/openshift/mysql/controller/StudentController.java
+++ src/main/java/com/codecraftshop/openshift/mysql/controller/StudentController.java
@@ -1,22 +1,69 @@
 package com.codecraftshop.openshift.mysql.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.codecraftshop.openshift.mysql.model.Student;
 import com.codecraftshop.openshift.mysql.repository.StudentRepository;
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
-	
+
 	@Autowired
 	private StudentRepository studentRepository;
-	
-	@RequestMapping(value="/students")
-	public List<Student> getStudents(){
+
+	// Create a new student
+	@PostMapping
+	public Student createStudent(@RequestBody Student student) {
+		return studentRepository.save(student);
+	}
+
+	// Get all students
+	@GetMapping
+	public List<Student> getAllStudents() {
 		return (List<Student>) studentRepository.findAll();
+	}
+
+	// Get a single student by ID
+	@GetMapping("/{id}")
+	public ResponseEntity<Student> getStudentById(@PathVariable(value = "id") Long studentId) {
+		Optional<Student> student = studentRepository.findById(studentId);
+		if(student.isPresent()) {
+			return ResponseEntity.ok().body(student.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	// Update a student by ID
+	@PutMapping("/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable(value = "id") Long studentId, @RequestBody Student studentDetails) {
+		Optional<Student> student = studentRepository.findById(studentId);
+		if(student.isPresent()) {
+			Student updatedStudent = student.get();
+			updatedStudent.setName(studentDetails.getName());
+			// Update other fields as necessary
+			final Student savedStudent = studentRepository.save(updatedStudent);
+			return ResponseEntity.ok(savedStudent);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+	// Delete a student by ID
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteStudent(@PathVariable(value = "id") Long studentId) {
+		Optional<Student> student = studentRepository.findById(studentId);
+		if(student.isPresent()) {
+			studentRepository.delete(student.get());
+			return ResponseEntity.ok().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
